0b199aa754314aeba0b681c27315804721725d5c
Matheus <mths@edu.unifor.br>
2021-07-13 19:44:46 -0300

    Estados

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { View, Text, StyleSheet, TextInput, Platform, TouchableOpacity } from 'react-native';
 
 export function Home() {
+    // conceitos de imutabilidade
+    // primeira posição é o estado, segunda posição é a função que atualiza o estado
+
+    //novas habilidades
+    const [newSkill, setNewSkill] = useState('');
+
+    // coleção das habilidades
+    const [mySkills, setMySkills] = useState([]);
+
+    // handle é uma convenção de quando a função é disparada por uma interação do usuario 
+    function handleAddNewSkill() {
+        setMySkills(oldState => [...oldState, newSkill]);
+    }
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome, Matheus</Text>
@@ -10,13 +25,33 @@ export function Home() {
                 style={styles.input}
                 placeholder="New skill"
                 placeholderTextColor="#555"
+                onChangeText={setNewSkill}
             />
 
-            <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-                <Text style={styles.buttonText}>Add</Text>
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+                onPress={handleAddNewSkill}
+            >
+                <Text style={styles.buttonText}>
+                    Add
+                </Text>
             </TouchableOpacity>
 
-            <Text style={[styles.title, { marginTop: 50 }]}>My Skill</Text>
+            <Text style={[styles.title, { marginVertical: 50 }]}>
+                My Skill
+            </Text>
+            
+            {
+                mySkills.map(skill => (
+                <TouchableOpacity key={skill}style={styles.buttonSkill}>
+                    <Text style={styles.textSkill}>
+                        {skill}
+                    </Text>
+                </TouchableOpacity>
+                ))
+            }
+
         </View>
     );
 }
@@ -47,10 +82,23 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 7,
         alignItems: 'center',
+        marginTop: 20,
     },
     buttonText: {
         color: '#fff',
         fontSize: 17,
         fontWeight: 'bold',
+    },
+    buttonSkill: {
+        backgroundColor: '#1F1E25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    textSkill: {
+        color: '#fff',
+        fontSize: 22,
+        fontWeight: 'bold',    
     }
 })
